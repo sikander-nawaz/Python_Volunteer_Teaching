dd1fc1a6f4ac50f0e2e36ee794951b2e005510ba
JadoGarBhai <sikandernawaz35@gmail.com>
2024-04-19 19:09:17 +0500

    'class_3'

---
+++ Class 3/main.py
@@ -0,0 +1,71 @@
+# # # # # # We are learning Python
+
+# # # # # """ a = 10
+# # # # # b = 5
+# # # # # c = 11
+
+# # # # # print(a,c)
+# # # # # print(a, c) """
+
+
+# # # # # # Escape Seaquence Words 
+
+# # # # # # This is a list of proucts:
+# # # # # # Mobiles
+# # # # # # Laptops
+
+# # # # # # Use of \n (for break line or new line)
+# # # # # print("This is a list of proucts:\n Mobiles \nLaptops ")
+
+# # # # # # Use of \t (for extra space like 4)
+# # # # # # word1    word2
+# # # # # print("word1\tword2")
+
+# # # # # # Use of \ (for uniqe words)
+# # # # # # omege code u03A9
+
+# # # # # # Befor \
+# # # # # print("u03A9")
+
+# # # # # # After \
+# # # # """ print("Ω")
+# # # # print("\u03A9")
+# # # # print("\u03A9")
+# # # # print("\u03A9")
+# # # # print("\u03A9") """
+
+# # # # print("Quid said: \"Work, work and work\"")
+
+
+# # # a = 10    #interger
+# # # b = "Hello"
+
+# # # print(b)
+# # # print("b")
+
+
+# # # Type Casting
+# # #with decimal(Integer)      without decimal(float)
+
+# # a = 45.67
+# # print("Float Data Type", a)
+
+# # b = int(a)
+# # print("Integer Data Type", b)
+
+# # print(a - b)
+
+# # # c = str(a)
+# # # print("String Data Type", c)
+
+
+# # How to check type of a variable/value
+# a = 23.45
+# b = 45
+# c = "H"
+# d = "Hello"
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))

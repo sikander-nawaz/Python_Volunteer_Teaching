0103a325a06763a99d6565aa378c244d4dce48d2
JadoGarBhai <sikandernawaz35@gmail.com>
2024-04-24 21:02:18 +0500

    'class-4'

---
+++ Class 5/main.py
@@ -0,0 +1,51 @@
+# # # # # a = 5
+# # # # # b = 10
+# # # # # # ">" is greater than
+# # # # # # c = a > b
+# # # # # # print(c)
+# # # # # # print(a > b)
+
+# # # # # # "<" is smaller than
+# # # # # print(a < b)
+
+# # # # # # ">=" is greater than or equal to
+# # # # a = 5
+# # # # b = 5
+# # # # # print(a >= b)
+
+# # # # # # "<=" is smaller than or equal to
+# # # # # print(a <= b)
+
+# # # # # # "==" is equal to
+# # # # # print(a == b)
+
+# # # # # # "!=" is not equal to
+# # # a = 5
+# # # b = 5
+# # # print(a != b)
+
+# # # assignment operator
+# # a = 5
+# # a = a + 5
+# # print("Normal way", a)
+
+# ######## # a += 5     # "ctrl + /"
+# # # print("Efficient Way", a)
+
+# # and    All situation true, answer will be true otherwise false
+# a = True
+# b = False
+# c = a and b
+# print("AND Operator", c)
+# # or
+# d = a or b
+# print( "OR operator ", d)
+# # not
+# e = not a 
+# print("NOT operator", e)
+
+
+a = True
+b = False
+
+print(a NOR b)

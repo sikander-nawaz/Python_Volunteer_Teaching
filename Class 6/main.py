4f61747e0b0555390954de3f06bb48815b8d95ab
JadoGarBhai <sikandernawaz35@gmail.com>
2024-04-25 21:13:39 +0500

    'class-6'

---
+++ Class 6/main.py
@@ -0,0 +1,41 @@
+# # # a = 5 
+# # # b = 4
+
+# # # if a > b:
+# # #      print("A is grather then B")
+# # # else:
+# # #      print("B is grather then A")
+
+# # age = int(input("Enter Your Age: "))
+
+# # if age > 18:
+# #      print("You're eligble")
+# #      print("You're eligble")
+# #      print("You're eligble")
+# #      print("You're eligble")
+# #      print("You're eligble")
+# # print("random")
+# # # else:
+# # #      print("Go back to your home")
+
+# passenger = input("Enter your path: ")
+
+# if passenger == "fsd":
+#      print("Go to the terminal 1")
+# elif passenger == "lahore":
+#      print("Go to the terminal 2")
+# elif passenger == "sahiwal":
+#      print("Go to the terminal 3")
+# else:
+#      print("We have only three terminals")
+
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+
+if age > 18:
+     if gender == "male":
+          print("Go to the both number 1")
+     else: 
+          print("go to the both number 2")
+else:
+     print("Go to the back home")

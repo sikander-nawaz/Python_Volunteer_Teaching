c3bdf6d8dd51f4566899df5127921c7293112b3d
JadoGarBhai <sikandernawaz35@gmail.com>
2024-05-06 21:02:28 +0500

    'class-10'

---
+++ Class 10/main.py
@@ -0,0 +1,11 @@
+# # str = "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc"  # length strt from 1 | index start from 0
+
+# # length = len(str)
+# # print(length)
+
+# str =  "Hello World"
+# # print(len(str))
+# # print(str[7])
+
+# newStr = str[0:5]    # 5-1 
+# print(newStr)

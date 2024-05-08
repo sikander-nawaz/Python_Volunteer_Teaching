2d93b581a6dba7423cf52a60e2dc113ce78428b2
JadoGarBhai <sikandernawaz35@gmail.com>
2024-05-08 21:22:46 +0500

    'class-12'

---
+++ Class 12/main.py
@@ -0,0 +1,75 @@
+# # # # # # # # # var = 1
+# # # # # # # # # # # var2 = "apple"
+# # # # # # # # # # # vare3 = 3
+
+# # # # # # # # # # # array = [1, 2, 3, 4]                    # array has only one data type
+# # # # # # # # # # abc = [1, "fruit", 23.45, True]     # list: collection of multiple data types or values.
+# # # # # # # # # # print(var)
+# # # # # # # # # # print(abc[2])
+
+# # # # # # # # # abc = [1, "fruit", 23.45, True, 1, "fruit", 23.45, True,1, "fruit", 23.45, True,1, "fruit", 23.45, True,1, "fruit", 23.45, True,1, "fruit", 23.45, True,1, "fruit", 23.45, True,1, "fruit", 23.45, True,1, "fruit", 23.45, True,1, "fruit", 23.45, True,]    
+
+# # # # # # # # # length = len(abc)   # length start from 1 & index start from 0
+# # # # # # # # # print(length)
+
+# # # # # # # # # print(len(abc))
+
+# # # # # # # list = ["apple", "mango", "orange"]
+# # # # # # # # list2 = [1,2,3]
+
+# # # # # # # # list.append("banana")   # add value at the end of list/
+
+# # # # # # # # list.insert(1, "banana")
+# # # # # # # print(list)
+
+# # # # # list1 = ["apple", "mango", "orange"]
+# # # # # list2 = ["banana", 1, 2, 3]
+
+# # # # # # # list[1] = "banana"
+
+# # # # # # list1.extend(list2)
+# # # # # print(list1 + list2)
+
+# # # # # # print(list1)
+
+# # # # # list = ["apple", "mango", "orange"]
+# # # # # list.pop(1)    # remove value from last when we've pop()
+
+# # # # # print(list)
+
+# # # # # list = ["apple", "mango", "orange", "mango"]
+
+# # # # # list.remove("mango")
+
+# # # # # print(list)
+
+# # # # list = ["apple", "mango", "orange"]
+
+# # # # list.clear()
+
+# # # # print(list)
+
+# # # # list = ["apple", "mango", "mango", "mango", "orange", "mango", "mango", "mango", "mango", "mango"]
+
+# # # # newList = list.copy() 
+
+# # # # print("Original List:", list)
+
+# # # # print("Copied List", newList)
+
+# # # # list = ["apple", "mango", "mango", "mango", "orange", "mango", "mango", "mango", "mango", "mango"]
+
+# # # # print(list.count("mango"))
+
+# # # list = [1, 2, 3, 4, 5]
+
+# # # list.reverse()
+
+# # # print(list)
+
+# list = ["banana", "apple", "orange", "mango"]    # asscending 1-9 | a-z
+
+# list.sort()
+# list.reverse()
+# print(list)
+
